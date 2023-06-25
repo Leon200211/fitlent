f9b6ea55c50eeb316d9232495e5b3d1ea6015da2
Leon200211 <leon200207@yandex.ru>
2023-06-25 14:46:36 +0300

    Подправил стили и почту

--- fitlent_site/public_html/mail.php
+++ fitlent_site/public_html/mail.php
@@ -3,18 +3,26 @@ $name = $_POST['name'];
 $phone = $_POST['phone'];
 $rates = $_POST['rates'];
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $to      = 'jokeycoffe@gmail.com';
+    $to      = 'fitlent@fitlent.ru';
     $subject = 'Новый заказ!';
     $message = 'Заказ от: '.$name.' Номер телефона: '.$phone.' По тарифу: '.$rates;
 
-    mail($to, $subject, $message);
+    $subject = '=?utf-8?b?'. base64_encode('Новый заказ!') .'?=';
+    $fromMail = 'info@fitlent.ru';
+    $fromName = 'Fitlent';
+    $date = date(DATE_RFC2822);
+    $messageId='<'.time().'-'.md5($fromMail.$to).'@'.$_SERVER['SERVER_NAME'].'>';
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= "Content-type: text/html; charset=utf-8". "\r\n";
+    $headers .= "From: ". $fromName ." <". $fromMail ."> \r\n";
+    $headers .= "Reply-To: info@fitlent.ru\r\n";
+    $headers .= "X-Mailer: PHP/".phpversion()."\r\n";
+    $headers .= "Date: ". $date ." \r\n";
+    $headers .= "Message-ID: ". $messageId ." \r\n";
+
+    mail($to, $subject, $message, $headers);
 }
 echo 1;
-
-
-
-
-
 
 
 ?>
