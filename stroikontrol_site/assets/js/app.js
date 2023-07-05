012e1609668548b71c44ef8c358f615fe645eeab
Greedushka <jokeycoffe@gmail.com>
2023-07-05 12:51:53 +0300

    Сверстал настин вариант

---
+++ stroikontrol_site/assets/js/app.js
@@ -0,0 +1,22 @@
+window.onload = () => {
+    const title = document.querySelector('.sitendapp--title')
+    const checkbox_1 = document.getElementById('color-1')
+    const checkbox_2 = document.getElementById('color-2')
+    const body = document.querySelectorAll('.siteandapp--body')
+    const radio = document.querySelectorAll('.radio')
+    radio.forEach(el => {
+        el.onchange = () => {
+            if (checkbox_1.checked) {
+                title.textContent = 'Сайт "СтройКонтроль"'
+                body[0].classList.remove('disabled')
+                body[1].classList.add('disabled')
+            } else if (checkbox_2.checked) {
+                title.textContent = 'Приложение "СтройКонтроль"'
+                body[1].classList.remove('disabled')
+                body[0].classList.add('disabled')
+            }
+        }
+    })
+
+}
+
